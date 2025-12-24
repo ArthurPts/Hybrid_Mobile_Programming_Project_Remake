879bdfd73019a447c4ff5e8e1f7517b9c94fa703
clath1a <alethiaclarita1@gmail.com>
2025-12-24 16:05:24 +0700

    fitur search berita

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { BeritaserviceService, Berita } from '../services/beritaservice.service';
+import { Component, inject } from '@angular/core';
+
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  standalone:false,
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    public beritaservice: BeritaserviceService //jangan ditanya kenapa begitu, tp emg begini dr ionicnya :v
+  ) {}
+  berita: any;
+  jenisTampilan: string = 'trending';
+  beritaDicari: string = '';
+  semuaBerita: any[] = [];
+  hasilPencarian: any[] = [];
+
+  cariBeritaByJudul() {
+    const lowerKeyword = this.beritaDicari.toLowerCase();
+    this.jenisTampilan = 'search';
+    if (!lowerKeyword) {
+      // kalau kosong, dia munculin semua berita
+      this.hasilPencarian = [...this.semuaBerita];
+    } else {
+      this.hasilPencarian = this.semuaBerita.filter((berita) =>
+        berita.judulBerita.toLowerCase().includes(lowerKeyword)
+      );
+    }
+  }
 
 }
